1eabac152e98aed553c8ff6c2fdaa9cf526a0305
Bethan Palmer <bethan.palmer@hotmail.com>
2021-12-05 08:39:11 +0000

    Add begin state for 03_02

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Integer[] scores = new Integer[]{80, 66, 73, 92, 43};
+
+    List<String> shoppingList = new ArrayList<>();
+    shoppingList.add("coffee");
+    shoppingList.add("bread");
+    shoppingList.add("pineapple");
+    shoppingList.add("milk");
+    shoppingList.add("pasta");
+
+  }
+}
