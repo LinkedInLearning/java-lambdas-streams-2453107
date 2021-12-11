13c5930e885a96cecf5742a05639a3a5507b4ebb
Bethan Palmer <bethan.palmer@hotmail.com>
2021-12-11 11:15:30 +0000

    Add end state for 03_02

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,13 +1,18 @@
 package com.example;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
 
   public static void main(String[] args) {
 
+    Stream<String> lettersStream = Stream.of("a", "b", "c");
+
     Integer[] scores = new Integer[]{80, 66, 73, 92, 43};
+    Stream<Integer> scoresStream = Arrays.stream(scores);
 
     List<String> shoppingList = new ArrayList<>();
     shoppingList.add("coffee");
@@ -15,6 +20,9 @@ public class Main {
     shoppingList.add("pineapple");
     shoppingList.add("milk");
     shoppingList.add("pasta");
+    Stream<String> shoppingListStream = shoppingList.stream();
+    shoppingListStream.sorted()
+            .forEach(item -> System.out.println(item));
 
   }
 }
