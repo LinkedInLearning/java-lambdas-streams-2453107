170e4684e7b47217d60440f769c3bfc761170b5d
Bethan Palmer <bethan.palmer@hotmail.com>
2021-12-12 19:03:49 +0000

    Add end state for 03_05

--- src/main/java/com/example/PlanetUtils.java
+++ src/main/java/com/example/PlanetUtils.java
@@ -1,11 +1,14 @@
 package com.example;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PlanetUtils {
 
   public List<Planet> getPlanetsBeginningWithM(List<Planet> planets) {
-    return null;
+    return planets.stream()
+            .filter(planet -> planet.getName().startsWith("M"))
+            .collect(Collectors.toList());
   }
 
   public List<Planet> getPlanetsWithRings(List<Planet> planets) {
